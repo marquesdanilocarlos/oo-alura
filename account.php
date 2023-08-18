737cf7eaae7910218e91d0d0d9fb6eacc02b2d50
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-08-18 00:07:04 -0300

    Aula 02 - Métodos

--- account.php
+++ account.php
@@ -7,13 +7,26 @@ $account->holderCpf = "033.218.441-25";
 $account->holderName = "Danilo Carlos Marques";
 $account->balance = 500;
 
+$account->withdraw(250);
+$account->deposit(137);
+
+var_dump($account);
+
 $anotherAccount = new Account;
 $anotherAccount->holderCpf = "044.321.541-22";
 $anotherAccount->holderName = "Juvenildo Santos";
 $anotherAccount->balance = 700;
 
+$anotherAccount->transfer(246, $account);
+
+$anotherAccount->withdraw(370);
+$anotherAccount->deposit(272);
+
+var_dump($account);
+var_dump($anotherAccount);
+
 
 $thirdAccount = $account;
 $thirdAccount->holderName = "Chris Bumstead, o CBUM!";
 
-var_dump($account, $anotherAccount, $thirdAccount);
+var_dump($thirdAccount);
--- src/Account.php
+++ src/Account.php
@@ -4,6 +4,40 @@ class Account
 {
     public string $holderCpf;
     public string $holderName;
-    public float $balance;
+    public float $balance = 0;
+
+    public function withdraw(float $value): void
+    {
+        if ($value > $this->balance) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->balance -= $value;
+        echo "Saque de {$value} realizado com sucesso! <br/>";
+    }
+
+    public function deposit(float $value): void
+    {
+        if ($value <= 0) {
+            echo "O valor a depositar deve ser um valor positivo.";
+            return;
+        }
+
+        $this->balance += $value;
+        echo "Depósito de {$value} realizado com sucesso! <br/>";
+    }
+
+    public function transfer(float $value, self $destinyAccount): void
+    {
+        if ($value > $this->balance) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->withdraw($value);
+        $destinyAccount->deposit($value);
+        echo "Transferência realizada com sucesso! <br/>";
+    }
 
 }
